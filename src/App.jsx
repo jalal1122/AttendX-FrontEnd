c56b95acdb77fb3d4b0076dd64428593527f8c1e
muhammad uzair <abuzada735@gmail.com>
2025-09-03 23:32:20 +0500

    Add LandingPage component and update App to render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Router from "./Components/Router";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
-      <Router />
+      <LandingPage />
+      {/* <Router /> */}
     </>
   );
 }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to AttendX</h1>
+      <p>Your one-stop solution for attendance management.</p>
+    </div>
+  )
+}
+
+export default LandingPage
